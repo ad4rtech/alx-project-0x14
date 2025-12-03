4bd8ebc4b10a64569f3ae426573214389c77bb51
Fidel Adar <adarfidel.ogw@gmail.com>
2025-12-03 17:20:32 +0300

    Add the movies pages to you project

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,8 +1,19 @@
-export default function MovieCard() {
-return (
-<div className="border rounded p-4 shadow">
-<h3 className="font-bold">Movie Title</h3>
-<p>Movie description here...</p>
-</div>
-);
+import { MovieProps } from "@/interfaces/index"
+import Image from "next/image"
+
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
 }
+
+export default MovieCard
